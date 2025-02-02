5df3dfdc3e18b193ac9b2a7a0e66bd788d99b9d8
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2025-02-02 17:19:33 +0530

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -2,7 +2,6 @@
 #link - https://leetcode.com/problems/two-sum/
 # concept - adding two numbers should give the target (less than o(n^2) time complexity)
  
-class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict1={}
         for i,j in enumerate(nums):
